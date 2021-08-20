ee0ae5ae334afdbde1263fdf11cf621f7b8e70e0
Glenn Musa <4622125+glennmusa@users.noreply.github.com>
2021-08-20 06:17:21 +0000

    pass arguments to scripts in cse

--- scripts/helloWorld.sh
+++ scripts/helloWorld.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "Hello, World!" >> /home/azureuser/helloWorld.txt
+host_to_sync="hostToSyncDefaultValue"
+
+echo "Hello, World! Here's where you set up synching to $host_to_sync! If Bicep magic worked, you should see a machine name. If it didn't, you'll see some default value." >> /home/azureuser/helloWorld.txt
